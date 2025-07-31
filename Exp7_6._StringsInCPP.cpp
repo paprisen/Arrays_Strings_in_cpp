f47575466bb5231d6daabc42bade639011dceae3
Papri <papri.sen2020@gmail.com>
2025-07-31 12:22:17 +0530

    Create Exp7_6._StringsInCPP.cpp

---
+++ Exp7_6._StringsInCPP.cpp
@@ -0,0 +1,23 @@
+//Papri Sen
+//24070123070
+//A3
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    char str1[10] = "Papri";
+    char str2[10] = {'S','I','T','\0'};
+    char str3[] = {'E','&','T','C','\0'};
+  
+    cout << "str1: " << str1 << endl;
+    cout << "str2: " << str2 << endl;
+    cout << "str3: " << str3 << endl;
+   
+    return 0;
+}
+/*
+Output:
+str1: Papri
+str2: SIT
+str3: E&TC
+*/
