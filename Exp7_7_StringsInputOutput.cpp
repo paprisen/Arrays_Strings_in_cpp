f11b09cdf017086342413d0084d71b60557654e0
Papri <papri.sen2020@gmail.com>
2025-07-31 12:24:47 +0530

    Create Exp7_7_StringsInputOutput.cpp

---
+++ Exp7_7_StringsInputOutput.cpp
@@ -0,0 +1,23 @@
+//Papri Sen
+//24070123070
+//A3
+#include<iostream>
+using namespace std;
+int main(){
+    char str1[100];
+    char str2[100];
+    cout << "Enter first string: ";
+    cin >> str1;
+    cout << "Enter second string: ";
+    cin >> str2;
+    cout << str1 << endl;
+    cout << str2 << endl;
+    return 0;
+}
+/*
+Output:
+Enter first string: Papri 
+Enter second string: Sen
+Papri
+Sen
+*/
