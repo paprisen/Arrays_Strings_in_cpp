dd2fbed73d9e0fdaff4b604403094f187b99314e
Papri <papri.sen2020@gmail.com>
2025-07-31 12:27:02 +0530

    Create Exp7_8_Concatenation.cpp

---
+++ Exp7_8_Concatenation.cpp
@@ -0,0 +1,28 @@
+// Papri Sen
+// 24070123070
+// A3
+#include<iostream>
+using namespace std;
+int main(){
+    string str1;
+    cout<<"enter the string1 = ";
+    cin>>str1;
+    string str2;
+    cout<<"enter the string2 = ";
+    cin>>str2;
+    string str3=str1 +str2;
+    cout<<str3<<endl;
+    
+    int n=str3.length();
+    cout<<"the lenght of the function is = "<<n<<endl;
+    
+    
+    return 0;
+}
+/*
+Output:
+enter the string1 = papri
+enter the string2 = sneha
+paprisneha
+the lenght of the function is = 10
+*/
